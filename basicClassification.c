30b77c2bd0f265f57823bf1ec33e64dc60991bbd
matanKlein <matan.Klein1@msmail.ariel.ac.il>
2024-01-22 23:50:27 +0200

    adding 1 to be prime

--- basicClassification.c
+++ basicClassification.c
@@ -22,6 +22,7 @@ int isStrong(int number){
 }
 
 int isPrime(int number){
+    if(number == 1){return true;}
     for (int i = 2; i*i <= number; i++)
     {
         if(number % i == 0 && i != number){
