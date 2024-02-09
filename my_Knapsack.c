0635f04d5bc51cf7eb50e37e5142f9b6077230d0
matanKlein <matan.Klein1@msmail.ariel.ac.il>
2024-02-09 14:03:29 +0200

    fixing the dynamic table

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -4,66 +4,102 @@
 int knapSack(int[],int[],int[]);
 
 int main(){
-    char* items[LENGTH];
+    char items[LENGTH][21];
     int values[LENGTH];
     int weights[LENGTH];
     int answer[LENGTH];
     char* result[LENGTH];
+    int maxProfit = 0;
     for (size_t i = 0; i < LENGTH; i++)
     {
-        char String[21];
-        scanf("%20s",String);
-        items[i] = String;
-        scanf("%d",&values[i]);
-        scanf("%d",&weights[i]);
+        scanf("%20s",items[i]);
+        scanf("%d", &values[i]);
+        scanf("%d", &weights[i]);
     }
-
-    int maxProfit = knapSack(weights,values,answer);
+    for (size_t i = 0; i < LENGTH; i++)
+    {
+        printf("%d " ,values[i]);
+    }
+    printf("\n");
+    for (size_t i = 0; i < LENGTH; i++)
+    {
+        printf("%d " ,weights[i]);
+    }
+    printf("\n");
+    for (size_t i = 0; i < LENGTH; i++)
+    {
+        printf("%s " ,items[i]);
+    }
+    printf("\n");
+    
+    maxProfit = knapSack(weights,values,answer);
+    printf("answers are: ");
     for (size_t i = 0; i < LENGTH; i++)
     {
         printf("%d",answer[i]);
     }
 
- //   printf("Maximum profit: %d\n",maxProfit);
+    printf("\nMaximum profit: %d \n",maxProfit);
  //   printf("Selected items:");
     for (size_t i = 0; i < LENGTH; i++)
     {
         if(answer[i] == 1){
             result[i] = items[i];
-            printf(" %s",items[i]);
+            printf(" %s",result[i]);
         }
     }
 }
 
 int knapSack(int weights[],int values[], int selected_bool[]){
-    int dynamicTable[LENGTH][WEIGHT] = {0};
+    int dynamicTable[LENGTH][WEIGHT+1];
+    
     for (size_t i = 0; i < LENGTH; i++)
     {
-        for (size_t j = 0; j < WEIGHT; j++)
-        {    
-            int withI;
-            int withOuti;
+        for (size_t j = 0; j <= WEIGHT; j++)
+        {
+            dynamicTable[i][j] = 0;
+        }
+        
+    }
+    
+    for (size_t i = 0; i < LENGTH; i++)
+    {
+        for (size_t j = 0; j <= WEIGHT; j++)
+        {   
+            int withI = 0;
+            int withOuti = 0;
+            int withIHelp;
             if(i == 0){
                if(weights[i] <= j){
                 dynamicTable[i][j] = values[i];
                }
+               continue;
+            }
+            withOuti = dynamicTable[i - 1][j];
+            withIHelp = j - weights[i];
+
+            if (withIHelp < 0){
+                withI = 0;
             }else{
-             withOuti = dynamicTable[i - 1][j];
-             if (j - weights[i] < 0)
-             {
-                 withI = 0;
-             }else{
-                withI = dynamicTable[i-1][j - weights[i]] + values[i];
+                withI = dynamicTable[i-1][withIHelp] + values[i];
             }
             if (withI < withOuti){
                 dynamicTable[i][j] = withOuti;
             }else {
                 dynamicTable[i][j] = withI;
-            }
-                }
-            
+            }  
+        }
     }
+    for (size_t i = 0; i < LENGTH; i++)
+    {
+        for (size_t j = 0; j <= WEIGHT; j++)
+        {
+            printf("%d ",dynamicTable[i][j]);
+        }
+        printf("\n");
+        
     }
+    ////////////////////////////////////////////////////////////////////////////////////
     for (size_t i = LENGTH - 1; i >= 0; i--){
     int j = 20;
     if(i == 0 || dynamicTable[i][j] == 0){
@@ -81,15 +117,6 @@ int knapSack(int weights[],int values[], int selected_bool[]){
         }
     
     }
-    for (size_t i = 0; i < LENGTH; i++)
-    {
-        for (size_t j = 0; j < WEIGHT; j++)
-        {
-            printf("%d ",dynamicTable[i][j]);
-        }
-        printf("\n");
-        
-    }
     
-    return dynamicTable[LENGTH][WEIGHT];
+    return dynamicTable[LENGTH-1][WEIGHT];
   }
